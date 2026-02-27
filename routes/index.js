987a24d5cf10e68de909b1d253b329b8d65d8aa6
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-27 08:28:13 +0530

    Routes for categories

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require("express");
 const router = Router();
+const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.getAllCategories);
 
 module.exports = router;
