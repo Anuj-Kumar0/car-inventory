e6eeff57cff088219b79af304a2e24652616e935
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-28 17:46:55 +0530

    Added Auth

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const router = Router();
 const carController = require("../controllers/carController");
+const verifyAdmin = require("../middleware/auth");
 
 router.get("/category/:id", carController.getCarsByCategory);
 router.post("/category/:id/cars", carController.createCar);
-router.post("/cars/:id/delete", carController.deleteCar);
-router.post("/cars/:id/update", carController.updateCar);
+router.post("/cars/:id/delete", verifyAdmin, carController.deleteCar);
+router.post("/cars/:id/update", verifyAdmin, carController.updateCar);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 const categoryController = require("../controllers/categoryController");
+const verifyAdmin = require("../middleware/auth");
 
 router.get("/", categoryController.getAllCategories);
 router.get("/category", (req, res) => {
     res.redirect("/");
   });
 router.post("/", categoryController.createCategory);
-router.post("/:id/update", categoryController.updateCategory);
-router.post("/:id/delete", categoryController.deleteCategory);
+router.post("/:id/update", verifyAdmin, categoryController.updateCategory);
+router.post("/:id/delete", verifyAdmin, categoryController.deleteCategory);
 
 module.exports = router;
