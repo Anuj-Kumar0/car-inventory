fcf9489f1c067fe4487f7aeaf0f37ff438b4d839
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-28 09:09:59 +0530

    Added routes for /categories

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = Router();
 const categoryController = require("../controllers/categoryController");
 
 router.get("/", categoryController.getAllCategories);
+router.get("/category", (req, res) => {
+    res.redirect("/");
+  });
 router.post("/", categoryController.createCategory);
-router.post(":id/update", categoryController.updateCategory);
+router.post("/:id/update", categoryController.updateCategory);
 router.post("/:id/delete", categoryController.deleteCategory);
 
 module.exports = router;
