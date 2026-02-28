b1241ccc5378556eb47e3a7dc5a7ad496bf9f5d6
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-28 09:07:02 +0530

    Use second route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("node:path");
 const assetsPath = path.join(__dirname, "public");
 const indexRouter = require('./routes/index')
-const carRouter = require("./routes/car");
+const carRouter = require("./routes/carRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -14,6 +14,7 @@ app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
+app.use("/", carRouter);
 
 app.use((req, res, next) => {
     res.status(404).render('partials/404');
