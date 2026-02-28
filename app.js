2bfba67192f19e71254dff2b4c9c91c2900782b2
Anuj Kumar <anujkumar21052004@gmail.com>
2026-02-28 17:45:20 +0530

    Modiefied Error Handling

--- app.js
+++ app.js
@@ -17,13 +17,21 @@ app.use("/", indexRouter);
 app.use("/", carRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('partials/404');
-  });
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
 
-  app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).render('partials/500');
-  });  
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  const status = err.status || 500;
+
+  if (status === 404) return res.status(404).render("partials/404");
+  if (status === 403) return res.status(403).render("partials/403");
+
+  res.status(500).render("partials/500");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
